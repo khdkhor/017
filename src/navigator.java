5db6096999e61685d71364c3bf2f93b8c12b2a01
zash <www.yasharp.m@gmail.com>
2021-09-14 22:53:46 -0700

    intial commit

---
+++ src/navigator.java
@@ -0,0 +1,20 @@
+/**
+ * this class navigator us to target classes
+ */
+public class navigator {
+
+
+    /**
+     * Let's goooo
+     * @param args
+     */
+
+    public static void main(String args[]) {
+
+
+
+
+        System.out.println("Navigator");
+    }
+
+}
